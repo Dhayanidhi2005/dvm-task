080db3f15d9aea0c67ed1fc761cc10878ec76442
DHAYANIDHI PURUSHOTHAMAN <f20231017@pilani.bits-pilani.ac.in>
2024-03-20 23:31:39 +0530

    pre-migrate models

--- professors/models.py
+++ professors/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
+from students.models import Students
 
 # Create your models here.
+
+class Department(models.Model):
+    department = models.CharField(max_length=50)
+
+    def __str__(self) :
+        return f"{self.department} Department"
+    
+class Branch(models.Model):
+    dept = models.ForeignKey(Department,on_delete=models.CASCADE)
+    branch = models.CharField(max_length=50)
+    branch_code = models.CharField(max_length=2)
+
+    def __str__(self) :
+        return f"{self.branch}({self.dept.department})"
+    
+class CourseList(models.Model):
+
+    cdc_choices = [
+        ("FY","First Year"),
+        ("SY1","Second Year Sem 1"),
+        ("SY2","Second Year Sem 1"),
+        ("TY1","Third Year Sem 1"),
+        ("TY2","Third Year Sem 2"),
+    ]
+
+    electives_choices = [
+        ("DEL","Department Elective"),
+        ("OPEL","Open Elective"),
+        ("HEL","Humanities Elective"),
+    ]
+
+    dept = models.ForeignKey(Department,on_delete=models.CASCADE)
+    credit = models.IntegerField()
+    course_id = models.CharField(max_length=10)
+    course_name = models.CharField(max_length=100)
+    cdcs = models.CharField(max_length=3,choices=cdc_choices,blank=True)
+    #so that in case of electrical dept we know it is cdc of which branch
+    branch = models.ForeignKey(Branch,on_delete=models.CASCADE)    
+    electives = models.CharField(max_length=4,choices=electives_choices,blank=True)
+
+class Courses(models.Model):
+    course = models.ForeignKey(CourseList,on_delete = models.CASCADE)    
+    student = models.ForeignKey(Students,on_delete=models.CASCADE)
+    marks = models.IntegerField()
+    grade=models.CharField(max_length=2,null=True)
+
+    def __str__(self):
+        return f"{self.course.course_name}:{self.student.bitsid}"
+    
+    class Meta:
+        verbose_name_plural = "Courses"
+
+class Professors(models.Model):
+
+    designation_choices = [
+        ("HOD","Head of Departemnt"),
+        ("PRO","Professor"),
+        ("AIP","Assistant Professor"),
+        ("AOP","Associate Professor"),
+    ]
+
+    prof = models.OneToOneField(User, on_delete=models.CASCADE)
+    designation = models.CharField(max_length=3,choices=designation_choices)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+    courses = models.ManyToManyField(CourseList,related_name="prof")
+
+    def __str__(self): 
+        return f"{self.prof.first_name } {self.prof.last_name}"
+    
+    class Meta:
+        verbose_name_plural = "Professors"
--- students/views.py
+++ students/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from professors.models import Courses
+from .models import Students
 
 # Create your views here.
 
+def curr_student(request):
+    curr_user = request.user
+    return Students.objects.get(user=curr_user)
+
 def home(request):
-    return HttpResponse("Welcome to students home.")
+    curr_user = curr_student(request)
+    user_courses = Courses.objects.filter(student=curr_user)
+    return render(request,"students/home.html",{
+        "courses":user_courses,
+    })
