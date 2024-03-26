43fe4a6965e74737f386bfa08afef411d8a05e45
DHAYANIDHI PURUSHOTHAMAN <f20231017@pilani.bits-pilani.ac.in>
2024-03-27 00:38:55 +0530

    modified settings.py

--- dvm/settings.py
+++ dvm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ("SECRET")
+SECRET_KEY = "django-insecure-a5xk@-#r7@%)43_9g)4#kz0poql0%6i6*5l+0k%n0p*$8r5b!-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
